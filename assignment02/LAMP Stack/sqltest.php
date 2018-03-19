4c13545571e9574a80c1fd715d3e8ec8df26b4a0
Chainshark <tommy.veevaete@hotmail.com>
2018-03-19 23:48:32 +0100

    edits

--- assignment02/LAMP Stack/sqltest.php
+++ assignment02/LAMP Stack/sqltest.php
@@ -1,10 +1,3 @@
-<?php
-# Fill our vars and run on cli
-# $ php -f db-connect-test.php
-$dbname = 'name';
-$dbuser = 'user';
-$dbpass = 'pass';
-$dbhost = 'localhost';
 $connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
 mysql_select_db($dbname) or die("Could not open the db '$dbname'");
 $test_query = "SHOW TABLES FROM $dbname";
